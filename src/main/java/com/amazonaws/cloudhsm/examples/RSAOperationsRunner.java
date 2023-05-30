fd309618f8484c51e7d1fd5c1e2bbcfab6151bc7
Sanjay Gandhi <sanjay.gandhi@jupiter.money>
2023-05-31 02:59:38 +0530

    Test

--- src/main/java/com/amazonaws/cloudhsm/examples/RSAOperationsRunner.java
+++ src/main/java/com/amazonaws/cloudhsm/examples/RSAOperationsRunner.java
@@ -176,7 +176,7 @@ public class RSAOperationsRunner {
 
         System.out.println("Encrypted plaintext = " + Base64.getEncoder().encodeToString(cipherText));
 
-        byte[] decryptedText = decrypt(transformation, privateKey, cipherText);
+        byte[] decryptedText = decrypt(transformation, kp.getPrivate(), cipherText);
         System.out.println("Decrypted text = " + new String(decryptedText, StandardCharsets.UTF_8));
 
         // RSA sign and verify.
